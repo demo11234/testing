98a8c7ef71ba2f2980e9dcfda3ec41147cb6cc7b
Mohan <100554588+mohan-chaudhari@users.noreply.github.com>
2022-04-25 18:33:11 +0530

    redis commented WIP:redis credentials needed in .env

--- shared/Constants.ts
+++ shared/Constants.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
 export const Constants = {
-    USER_TOKEN_VALIDITY : "24h",
-    JWT_SECRET_KEY : process.env.JWT_SECRET_KEY
-} 
+  USER_TOKEN_VALIDITY: '24h',
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+};
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,4 @@
-import {
-  CACHE_MANAGER,
-  Inject,
-  Injectable,
-} from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -12,10 +8,8 @@ import { WalletAddressDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UserService {
-
   constructor(
-    private readonly userRepository: UserRepository,
-    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    private readonly userRepository: UserRepository, //  @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {}
 
   /**
@@ -24,10 +18,10 @@ export class UserService {
    * @returns it will return user details
    * @author Jeetanshu Srivastava
    */
-   async createUser(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
     try {
       const user = this.userRepository.createUser(createUserDto);
-      return user;        
+      return user;
     } catch (error) {
       throw new Error(error);
     }
@@ -44,14 +38,14 @@ export class UserService {
       const { walletAddress } = walletAddressDto;
 
       //Fetching user info from cache
-      const cachedItem = await this.cacheManager.get(walletAddress);
-      if(cachedItem) return cachedItem;
+      // const cachedItem = await this.cacheManager.get(walletAddress);
+      // if (cachedItem) return cachedItem;
 
       const user = await this.userRepository.findUser(walletAddress);
       if (!user) return null;
 
       //Setting user info into the cache
-      await this.cacheManager.set(walletAddress, user);
+      // await this.cacheManager.set(walletAddress, user);
       return user;
     } catch (error) {
       throw new Error(error);
@@ -64,46 +58,45 @@ export class UserService {
    * @returns it will return user details or null
    * @author Jeetanshu Srivastava
    */
-    async findUserByUserName(userName: string): Promise<any> {
+  async findUserByUserName(userName: string): Promise<any> {
     try {
       //Fetching user info from cache
-      const cachedItem = await this.cacheManager.get(userName);
-      if(cachedItem) return cachedItem;
+      // const cachedItem = await this.cacheManager.get(userName);
+      // if (cachedItem) return cachedItem;
 
       const user = await this.userRepository.findUserByUserName(userName);
       if (!user) return null;
 
       //Setting user info into the cache
-      await this.cacheManager.set(userName, user);
+      // await this.cacheManager.set(userName, user);
       return user;
     } catch (error) {
       throw new Error(error);
     }
   }
 
-    /**
+  /**
    * @description findUserByEmail will return user details with given email
    * @param email
    * @returns it will return user details or null
    * @author Jeetanshu Srivastava
    */
-     async findUserByEmail(email: string): Promise<any> {
-      try {
-        //Fetching user info from cache
-        const cachedItem = await this.cacheManager.get(email);
-        if(cachedItem) return cachedItem;
-  
-        const user = await this.userRepository.findUserByEmail(email);
-        if (!user) return null;
-  
-        //Setting user info into the cache
-        await this.cacheManager.set(email, user);
-        return user;
-      } catch (error) {
-        throw new Error(error);
-      }
+  async findUserByEmail(email: string): Promise<any> {
+    try {
+      //Fetching user info from cache
+      // const cachedItem = await this.cacheManager.get(email);
+      // if (cachedItem) return cachedItem;
+
+      const user = await this.userRepository.findUserByEmail(email);
+      if (!user) return null;
+
+      //Setting user info into the cache
+      // await this.cacheManager.set(email, user);
+      return user;
+    } catch (error) {
+      throw new Error(error);
     }
-  
+  }
 
   /**
    * @description update will update the user details
@@ -111,13 +104,19 @@ export class UserService {
    * @returns it will return user details
    * @author Jeetanshu Srivastava
    */
-  async update(walletAddress: string, updateUserDto: UpdateUserDto): Promise<any> {
+  async update(
+    walletAddress: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<any> {
     try {
-      let user = await this.userRepository.updateUserDetails(walletAddress, updateUserDto);
+      let user = await this.userRepository.updateUserDetails(
+        walletAddress,
+        updateUserDto,
+      );
       if (!user) return null;
 
       //Setting user info into the cache
-      await this.cacheManager.set(walletAddress, user);
+      // await this.cacheManager.set(walletAddress, user);
       return user;
     } catch (error) {
       throw new Error(error);
