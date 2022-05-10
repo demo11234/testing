6e23aafd751236951675da36b70e38b5435971ab
khageshdevadi <87413560+khageshdevadi@users.noreply.github.com>
2022-05-10 15:13:41 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd /home/ubuntu/backend
-sudo cp /home/ubuntu/.env ./
 sudo pm2 stop all
 sudo pm2 delete all
+sudo -i
+sudo rm ./.pm2/logs/*
+exit
+cd /home/ubuntu/backend
+sudo cp /home/ubuntu/.env ./
 sudo pm2 start "./dist/src/main.js"
