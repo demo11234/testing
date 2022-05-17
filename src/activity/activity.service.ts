471c81764fa11deb91f4a50b8f5e1b752008c698
Jeetanshu <jeetanshu18srivastava@gmail.com>
2022-05-17 14:51:26 +0530

    Updated Query to Find Activity by Item Id

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -139,12 +139,11 @@ export class ActivityService {
     try {
       const activity = await this.activityRepository
         .createQueryBuilder('activity')
-        .leftJoinAndSelect('activity.nftItem', 'nftItem', 'nftItem.id = :id', {
-          id,
-        })
         .leftJoinAndSelect('activity.fromAccount', 'fromAccount')
         .leftJoinAndSelect('activity.toAccount', 'toAccount')
         .leftJoinAndSelect('activity.winnerAccount', 'winnerAccount')
+        .leftJoinAndSelect('activity.nftItem', 'nftItem')
+        .where('nftItem.id = :id', { id })
         .getMany();
       return activity;
     } catch (error) {
